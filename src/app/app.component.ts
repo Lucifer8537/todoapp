a369c691bbe01af0a2e1c137c6f3d6addb20582e
navodit <navodit50@gmail.com>
2024-02-11 08:40:38 +0530

    Update in assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-app';
+  newTask!: string;
+  isTaskCreationFinal = false;
+  onCreateTask = () => {
+    console.log('newTask : ', this.newTask);
+    this.newTask = '';
+    if (this.isTaskCreationFinal) this.isTaskCreationFinal = false;
+  };
 }
